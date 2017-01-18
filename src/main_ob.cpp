ddb7e14a43b45d43c551f4143ece370552889540
Anders S. Christensen <andersx@nano.ku.dk>
2017-01-18 17:24:17 +0100

    Working tranlation move

--- src/main_ob.cpp
+++ src/main_ob.cpp
@@ -15,11 +15,11 @@
 #include <openbabel/forcefield.h>
 #include <openbabel/obutil.h>
 
-
 int main(int argc, char *argv[]) {
 
     // OpenBabel::OBPlugin::List("forcefields", "verbose");
-    std::string ff = "MMFF94";
+    // std::string ff = "MMFF94";
+    std::string ff = "UFF";
     const int steps = 200;
     const double crit = 5.0e-4;
 
@@ -40,6 +40,14 @@ int main(int argc, char *argv[]) {
     std::ifstream ifs;
     ifs.open(base_file.c_str());
     conv.Read(&mol, &ifs);
+    ifs.close();
+
+
+    OpenBabel::OBMol mol2;
+
+    ifs.open(dock_file.c_str());
+    conv.Read(&mol2, &ifs);
+    ifs.close();
 
     OpenBabel::OBForceField* pFF = OpenBabel::OBForceField::FindForceField(ff);
     if (!pFF) {
@@ -50,33 +58,83 @@ int main(int argc, char *argv[]) {
     pFF->SteepestDescentInitialize(steps, crit);
 
     double e = pFF->Energy();
-    std::cout << e << std::endl;
+    // std::cout << e << std::endl;
     unsigned int total_steps = 0;
     bool done = true;
     // bool done = false;
+    // conv.Write(&mol, &std::cout);
     while (done) {
         done = pFF->SteepestDescentTakeNSteps(1);
         total_steps++;
 
       if (pFF->DetectExplosion()) {
         std::cerr << "explosion has occured!" << std::endl;
-        conv.Write(&mol, &std::cout);
+        // conv.Write(&mol, &std::cout);
         return(1);
       } else
         pFF->GetCoordinates(mol);
     }
-    std::cout << total_steps << std::endl;
+    // std::cout << total_steps << std::endl;
+    e = pFF->Energy();
+    // std::cout << e << std::endl;
+    // conv.Write(&mol, &std::cout);
+   
+    e = pFF->Energy();
+    // std::cout << e << std::endl;
+
+    OpenBabel::OBAtom *atom;
+    std::ofstream ofs("out.xyz");
+
+    mol += mol2;
+    pFF->Setup(mol);
+    conv.Write(&mol, &std::cout);
+    conv.Write(&mol, &ofs);
     e = pFF->Energy();
     std::cout << e << std::endl;
-    conv.Write(&mol, &std::cout);
-   
-    OpenBabel::OBAtom *atom;
 
-    for (OpenBabel::OBMolAtomIter a(mol); a; ++a ) {
-      atom = mol.GetAtom(a->GetIdx());
-      std::cout << atom->GetVector() << std::endl;
+    double energy_new = e;
+    double energy_old = energy_new;
+
+    OpenBabel::OBMol mol_old = mol;
+    mol_old.SetCoordinates(mol.GetCoordinates());
+
+    for (unsigned int step = 0; step < 1000; step++) {
+
+        OpenBabel::vector3 move;
+        move.randomUnitVector();
+
+        for (unsigned int i = mol.NumAtoms() - mol2.NumAtoms() + 1;
+            i < mol.NumAtoms() + 1; i++) {
+            atom = mol.GetAtom(i);
+            atom->SetVector(atom->GetVector() + move);
+        }
+
+        pFF->SetCoordinates(mol);
+        e = pFF->Energy();
+
+        conv.Write(&mol, &ofs);
+
+        if (e < energy_old) {
+
+            mol_old.SetCoordinates(mol.GetCoordinates());
+            energy_old = e;
+            std::cout << e << "    accept" << std::endl;
+        } else {
+            mol.SetCoordinates(mol_old.GetCoordinates());
+            e = energy_old;
+            std::cout << e << "    reject" << std::endl;
+        }
     }
-
+   
+   
+   
+   
+    ofs.close();
+    // e = pFF->Energy();
+    // std::cout << e << std::endl;
+    // pFF->SetCoordinates(mol);
+    // e = pFF->Energy();
+    // std::cout << e << std::endl;
     // Initialize energy function
 
     // Minimize monomer1
